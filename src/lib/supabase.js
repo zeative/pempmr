372db477e99abf67d956ac7eeeaee3b395ac7479
zeative <zaadevofc@gmail.com>
2025-08-21 19:35:48 +0700

    fix: env access

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
